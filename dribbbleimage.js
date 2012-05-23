5b671186dd7596000d672101ec10b41c5f46b287
passionke <kejiiqing@yahoo.com.cn>
2012-05-23 08:27:07 +0800

    modifiy

--- dribbbleimage.js
+++ dribbbleimage.js
@@ -34,15 +34,17 @@ var dribbbleRule = {
 		else
 			return false;
 	},
-	links:(function(maxuse){
-		var links = [];
-		var baseUrl = 'http://dribbble.com/shots/popular/';
-		links.push(baseUrl);
-		for (var i = 2; i<maxuse; i++){
-			links.push(baseUrl + '?page=' + i);
-		}
-		return links;
-	})(10),
+	results:{	
+		link:(function(maxuse){
+			var links = [];
+			var baseUrl = 'http://dribbble.com/shots/popular/';
+			links.push(baseUrl);
+			for (var i = 2; i<maxuse; i++){
+				links.push(baseUrl + '?page=' + i);
+			}
+			return links;
+		})(3)
+	},
 	method:'serial',
 	selector:"$(\".extras img[alt=\'Attachments\']\").closest(\'.dribbble\').find(\'a.dribbble-over\')",
 	target:'href',
@@ -50,10 +52,30 @@ var dribbbleRule = {
 		selector:'$(\'.attachments ul li a\')',
 		target:'href',
 		method:'serial',
+		info:[
+			{
+				key:'title',
+				keyselector:'$(\'#screenshot-title\')'
+			},
+			{
+				key:'tags',
+				keyselector:'$(\'#tags li strong\').text(function(i,c){return c+\',\'}).text().replace(\',,\',\',\')'
+			},{
+				key:'color',
+				keyselector:'$(\'.color-chips li a\').text(function(i,c){return c+\',\'}).text().replace(\',,\',\',\')'
+			}
+		],
 		nextRule:{
 			selector:'$(\'#viewer-img img\')',
 			target:'src',
-			method:'parallel'
+			info:[
+			{
+				key:'imagelink',
+				keyselector:'$(\'#viewer-img img\').attr(\'src\')'
+			}			
+			],
+			method:'parallel',
+			save:true
 		}
 	}
 }
@@ -61,7 +83,7 @@ var dribbbleFilter = function(url, rule, callback){
 	if (url == undefined) return;
 	var htmlfilter = function(url, rule){
 		var jsdom = require('jsdom');
-		console.log('fetching data at ' + url);
+		//console.log('fetching data at ' + url);
 		request({ uri:url }, function (error, response, body) {
 		  if (error) {
 			console.log('Error when connect to ' + url);
@@ -77,7 +99,11 @@ var dribbbleFilter = function(url, rule, callback){
 		  	}
 			var $ = window.jQuery;
 			var queue = eval(rule.selector);
-			var result = [];
+			if (rule.results == undefined){
+				rule.results = {};			
+				rule.results.info = [];				
+			}
+			rule.results.link = [];
 			for (var i = 0; i< queue.length; i ++){
 				var link = $(queue[i]).attr(rule.target);
 				var args = url.match(/^(([a-z]+):\/\/)?([^\/\?#]+)\/*([^\?#]*)\??([^#]*)#?(\w*)$/i);  
@@ -91,10 +117,16 @@ var dribbbleFilter = function(url, rule, callback){
 				if (link.indexOf(parase['host']) < 0){
 					link = parase['schema'] + '://' + parase['host'] + link;
 				}
-				result.push(link);			
+				rule.results.link.push(link);			
 			}
-			console.log(result);
-			callback(result);
+			if (rule.info != undefined){
+				for (i = 0; i < rule.info.length; i++){
+					var temp = {}
+					temp[rule.info[i].key] = eval(rule.info[i].keyselector);
+					rule.results.info.push(temp);
+				}
+			}
+			callback(rule.results.Clone());
 		  });
 		});	
 	}
@@ -103,52 +135,42 @@ var dribbbleFilter = function(url, rule, callback){
 var dribbbleEngine = function(dribbbleRule){
 	if (dribbbleRule == undefined) return;	
 	var caller = dribbbleEngine.caller;
-	var urls = dribbbleRule.links;
+	var urls = dribbbleRule.results.link || dribbbleRule.links;
+	var async = require('async');
 	var rule = {};
 	rule.selector = dribbbleRule.selector;
 	rule.target = dribbbleRule.target;
-	var parallelMode = function(){
-		for (var i = 0; i < urls.length; i ++ ){
-			if (dribbbleRule.inspector != undefined){
-				dribbbleRule.inspector.sendout.push(urls[i]);
-			}
-			new dribbbleFilter(urls[i], rule, (
-				function(link){
-					return function(links){
-						if (dribbbleRule.nextRule != undefined){
-							var currentRule = dribbbleRule.nextRule.Clone();
-							currentRule.links = links;
-							currentRule.inspector = dribbbleRule.inspector.Clone();
-							new dribbbleEngine(currentRule);
-						}
-						dribbbleRule.inspector.emit('done', link);
-					}
-				})(urls[i]));	
-		}
-		return;
+	rule.info = dribbbleRule.info;	
+	var automtask = function(url, rule){
+		var that = {};
+		that.url = url;
+		that.rule = rule;
+		return function(callback){
+			//console.log(that);
+			new dribbbleFilter(that.url, that.rule, function(results){
+				if (dribbbleRule.nextRule != undefined){
+					var currentRule = dribbbleRule.nextRule.Clone();
+					currentRule.results = results;
+					new dribbbleEngine(currentRule);					
+				}
+				callback(null, results);
+			});
+			
+		};
+	};
+	
+	var tasklist = [];
+	for (var i = 0; i < urls.length; i ++ ){			
+		tasklist.push(new automtask(urls[i], rule));
 	}
-	var serialMode = function(){
-		new dribbbleFilter(urls.pop(), rule, function(links){
-			if (dribbbleRule.nextRule != undefined){
-				var currentRule = dribbbleRule.nextRule.Clone();
-				currentRule.links = links;
-				currentRule.inspector = inspector;
-				new dribbbleEngine(currentRule);
-			}
-		});
-		var inspector = new events.EventEmitter();
-		inspector.sendout = [];
-		inspector.addListener('done', function(link){
-			inspector.sendout.pop();
-			if (inspector.sendout.length == 0){
-				new serialMode();
-			}
-		});		
-	}	
 	if (dribbbleRule.method == 'parallel'){
-		parallelMode();
+		async.parallel(tasklist, function(err, results){
+			//console.log(results);
+		});
 	}else{
-		new serialMode();
+		async.series(tasklist, function(err, results){
+			console.log(JSON.stringify(results));
+		});
 	}	
 }
-new dribbbleEngine(dribbbleRule);
+dribbbleEngine(dribbbleRule);
